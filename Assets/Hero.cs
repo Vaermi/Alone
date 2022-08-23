ffb7822b60a6298d6805f45b1f444fbeebf716cb
Doris Drye <dorisdrye@hotmail.com>
2022-08-23 21:27:34 +0200

    add hero

---
+++ Assets/Hero.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        Vector3 force = new Vector3();
+
+        if (Input.GetKey("w"))
+        {
+
+        }
+        
+    }
+}
