2a772c1f1bac288597fc95f53934bcdc5ed1e6a3
Vaermi <dorisdrye@hotmail.com>
2022-08-24 21:14:42 +0200

    add hero tile, add movement to hero.cs

--- Assets/Game/Scripts/Hero.cs
+++ Assets/Game/Scripts/Hero.cs
@@ -7,14 +7,30 @@ public class Hero : MonoBehaviour
 
     // Update is called once per frame
     
-    void Update()
+    private void Update()
     {
-        Vector3 force = new Vector3();
+        Vector3 change = new Vector3();
 
-        if (Input.GetKey("w"))
+        if (Input.GetKey(KeyCode.D))
         {
+            change.x = 1f;
 
+        } else if (Input.GetKey(KeyCode.W))
+        {
+            change.y = 1f;
+
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            change.y = -1f;
+
+        } else if ( Input.GetKey(KeyCode.A))
+        {
+            change.x = -1f;
         }
+            
+        transform.position += change * Time.deltaTime;
+        
+
         
     }
 }
