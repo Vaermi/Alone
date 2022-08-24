9a2a247f9b368a191063331de09bef22d1446e41
Vaermi <dorisdrye@hotmail.com>
2022-08-24 22:47:20 +0200

    rename change with move, add gamepad controlls

--- Assets/Game/Scripts/Hero.cs
+++ Assets/Game/Scripts/Hero.cs
@@ -5,30 +5,33 @@ using UnityEngine;
 public class Hero : MonoBehaviour
 {
 
-    // Update is called once per frame
+    // Update wird einmal pro Frame aufgerufen
     
     private void Update()
     {
-        Vector3 change = new Vector3();
+        /* Movement für das Hero Objekt  für W,A,S,D / Arrow up, right, down, left / und Gamepad Left-Stick (über Hinput Gamepad Manager), 
+         Frame-rate unabhängige Bewegung durch deltaTime */
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            change.x = 1f;
+        Vector3 move = new Vector3();
 
-        } else if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || Hinput.gamepad[0].leftStick.right)
         {
-            change.y = 1f;
+            move.x = 1f;
 
-        } else if (Input.GetKey(KeyCode.S))
+        } else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Hinput.gamepad[0].leftStick.up)
         {
-            change.y = -1f;
+            move.y = 1f;
 
-        } else if ( Input.GetKey(KeyCode.A))
+        } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Hinput.gamepad[0].leftStick.down)
         {
-            change.x = -1f;
+            move.y = -1f;
+
+        } else if ( Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Hinput.gamepad[0].leftStick.left)
+        {
+            move.x = -1f;
         }
             
-        transform.position += change * Time.deltaTime;
+        transform.position += move * Time.deltaTime;
         
 
         
