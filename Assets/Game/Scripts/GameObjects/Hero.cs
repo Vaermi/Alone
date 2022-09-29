aff23e90237db96b24381f1949b9e0ec6b22cc47
Vaermi <dorisdrye@hotmail.com>
2022-09-29 18:01:52 +0200

    add variables and constructor to hero.cs

--- Assets/Game/Scripts/GameObjects/Hero.cs
+++ Assets/Game/Scripts/GameObjects/Hero.cs
@@ -8,8 +8,43 @@ using UnityEngine.Tilemaps;
 /// </summary>
 public class Hero : TheGameObject
 {
+    string name;
+    int health;
+    int maxHealth;
+    int sanity;
+    int maxSanity;
+    int attack;
+    int maxAttack;
 
+    public Hero()
+    {
+
+    }
+    public Hero(string name)
+    {
+        this.name = name;
+    }
    
+    public Hero(string name, int health, int sanity, int attack)
+    {
+        this.name = name;
+        this.health = health;
+        this.sanity = sanity;
+        this.attack = attack;
+    }
+
+    public Hero(string name, int health,int maxHealth, int sanity,int maxSanity, int attack, int maxAttack)
+    {
+        this.name = name;
+        this.health = health;
+        this.maxHealth = maxHealth;
+        this.sanity = sanity;
+        this.maxSanity = maxSanity;
+        this.attack = attack;
+        this.maxAttack = maxAttack;
+    }
+
+
     
 
 
