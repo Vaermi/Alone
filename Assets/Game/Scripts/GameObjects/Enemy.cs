fc04712189e41fbda848bcb23087eef2520d2764
Vaermi <dorisdrye@hotmail.com>
2022-10-11 22:53:02 +0200

    add Enemy.cs, add new method fight to hero.cs

---
+++ Assets/Game/Scripts/GameObjects/Enemy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : TheGameObject
+{
+    public string EnemyName;
+    public int Health;
+    public int Defence;
+    public int Attack;
+    public int AttackSpeed;
+    public int DefaultDice = 12;
+}
--- Assets/Game/Scripts/GameObjects/Hero.cs
+++ Assets/Game/Scripts/GameObjects/Hero.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -12,15 +13,16 @@ public class Hero : TheGameObject
 {
     public QuestObjects questObjects;
     public QuestPanel panel;
+    private Enemy enemy;
 
     public string HeroName = "Doris";
     public int Health = 50;
-    public int Sanity = 0;
+    public int Insanity = 0;
     public int Inventory = 0;       //Counter für Inventory/Placeholder
-    int maxHealth = 100;    
-    int maxSanity = 100;
-    int attack = 250;
-    int maxAttack = 1000;
+    public int Defence = 20;
+    public int Attack = 5;
+    public int AttackSpeed = 3;
+    public int DefaultDice = 10;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -30,7 +32,56 @@ public class Hero : TheGameObject
 
     }
 
+    private void Fight()
+    {
+        float heroDice = Random.Range(0, DefaultDice);
+        float enemyDice = Random.Range(0, enemy.DefaultDice);
 
+        if(AttackSpeed > enemy.AttackSpeed)
+        {
+            Debug.Log("Du beginnst...");
+            while(Health != 0 || enemy.Health != 0)
+            {
+                int counter = 0;
+
+                Debug.Log($"Runde {counter}");
+                float heroDmgOutput = Attack * heroDice;
+                float enemyDmgInput = heroDmgOutput - enemy.Defence;
+                enemy.Health -= (int)enemyDmgInput;
+                counter++;
+                if(enemy.Health <= 0) Debug.Log("Du hast gewonnen!");
+
+                Debug.Log($"Runde {counter}");
+                float enemyDmgOutput = enemy.Attack * enemyDice;
+                float heroDmgInput = enemyDmgOutput - Defence;
+                Health -= (int)heroDmgInput;
+                counter++;
+                if(Health <= 0) Debug.Log($"Du bist tot.\n GAME OVER");                
+            }
+
+        } else
+        {
+            Debug.Log("Enemy beginnt...");
+            while (Health != 0 || enemy.Health != 0)
+            {
+                int counter = 0;
+
+                Debug.Log($"Runde {counter}");
+                float enemyDmgOutput = enemy.Attack * enemyDice;
+                float heroDmgInput = enemyDmgOutput - Defence;
+                Health -= (int)heroDmgInput;
+                counter++;
+                if (Health <= 0) Debug.Log($"Du bist tot.\n GAME OVER");
+
+                Debug.Log($"Runde {counter}");
+                float heroDmgOutput = Attack * heroDice;
+                float enemyDmgInput = heroDmgOutput - enemy.Defence;
+                enemy.Health -= (int)enemyDmgInput;
+                counter++;
+                if (enemy.Health <= 0) Debug.Log("Du hast gewonnen!");
+            }
+        }
+    }
 
 
 
