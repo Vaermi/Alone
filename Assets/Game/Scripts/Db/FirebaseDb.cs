f2281461dfe90f2573bddb3252f322df22660c48
Doris Drye <dorisdrye@hotmail.com>
2022-10-18 21:36:50 +0200

    firebase scripts addet

---
+++ Assets/Game/Scripts/Db/FirebaseDb.cs
@@ -0,0 +1,17 @@
+using Firebase.Firestore;
+using UnityEngine;
+
+public class FirebaseDb : MonoBehaviour
+{
+    //TODO Spielername hinzufügen
+    //TODO Spielername lesen
+    //TODO Quest abrufen
+    //TODO Questfortschritt speichern
+    //TODO Spielerposition speichern
+    //TODO Spielerposition abrufen
+    //TODO Spielerwerte speichern
+    //TODO Spielerwerte abrufen
+    //TODO Spielstand speichern
+    //TODO Spielstand laden
+    //TODO Spielstand löschen
+}
