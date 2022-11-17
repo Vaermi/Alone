d4e9bb6dec02313981c66c137fdfecc5d8cad2b3
Doris Drye <dorisdrye@hotmail.com>
2022-11-17 18:39:03 +0100

    new button in menu, add quitgame method, add input for menu

---
+++ Assets/Game/Scripts/UIController/GameQuitController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameQuitController : MonoBehaviour
+{
+
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+}
