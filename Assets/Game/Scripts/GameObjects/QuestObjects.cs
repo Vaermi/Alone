5de8094dc3ca62bb55adb137a8bd6f122cc62d28
Vaermi <dorisdrye@hotmail.com>
2022-11-24 18:03:00 +0100

    questObject fixes

--- Assets/Game/Scripts/GameObjects/QuestObjects.cs
+++ Assets/Game/Scripts/GameObjects/QuestObjects.cs
@@ -6,7 +6,9 @@ public class QuestObjects : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
     public string QuestId;
+    public string NextQuestId;
     public string QuestText { get; private set; }
+    public bool isActive = false;
 
     public QuestPanelController questPanel;
 
@@ -18,16 +20,24 @@ public class QuestObjects : MonoBehaviour
         spriteRenderer.enabled = !spriteRenderer.enabled;
         Debug.Log("Switch Status");
         GameObject.Find("TextQuests").GetComponent<QuestController>().LoadQuest(QuestId);
+        isActive = false;
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name is not "Hero") return;
-
+        
         Debug.Log("Trigger");
         questPanel.SetQuestWindowActive();
         SwitchStatusQuestObjects();
+    }
+
+
+    public void SwitchToNextQuest()
+    {
+        QuestId = NextQuestId;
+        isActive = true;
     }
 
 }
